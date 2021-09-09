bc727927ff2445eb8748bd62d847c948d42190a6
Matt Derocher <mjamesderocher@gmail.com>
2021-09-09 06:04:08 +0300

    Added Netlify adapter

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,57 +3,37 @@
 </script>
 
 <script>
-	import Counter from '$lib/Counter.svelte';
+	let html = '<p>Write some text!</p>'
+	let selection = undefined
+
+	const handleSelection = (e) => {
+		console.log(e)
+	}
+
+	const makeBold = (e) => {
+		document.execCommand('bold')
+	}
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
+<nav class="toolbar">
+	<button id='bold_btn' on:click={e => makeBold(e)}>B</button>
+	<button id='italic_btn'>I</button>
+</nav>
+
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+	<div 
+		contenteditable="true" 
+		bind:innerHTML={html}
+		on:keyup={e => console.log(e)}
+	></div>
 </section>
 
 <style>
-	section {
+	.toolbar {
 		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
 /** @type {import('@sveltejs/kit').Config} */
+import netlify from '@sveltejs/adapter-netlify'
+
 const config = {
 	kit: {
+		adapter: netlify(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}

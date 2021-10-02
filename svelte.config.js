6e8abc47a9a24e383738eb0199459fc7c0459d8b
Matt Derocher <mjamesderocher@gmail.com>
2021-10-02 07:57:03 +0300

    Fixed name of service worker

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		files: {
-			serviceWorker: 'src/service-worker.ts',
+			serviceWorker: 'src/service-worker.js',
 		},
 		vite: {
 			ssr: {

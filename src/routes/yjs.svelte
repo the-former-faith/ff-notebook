816ee170f1170925e53a31d9dd9e7222f17cf2b8
Matt Derocher <mjamesderocher@gmail.com>
2021-10-04 08:39:40 +0300

    Added yjs test page

---
+++ src/routes/yjs.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { browser } from '$app/env'
+  import * as Y from 'yjs'
+
+  // A Yjs document holds the shared data
+  const ydoc = new Y.Doc()
+  // This Y.Array contains a list of documents represented as Y.Text
+  const documentList = ydoc.getMap('doc-list')
+
+  documentList.observe( ( event => console.log(event) ) )
+
+  const createDoc = () => {
+    
+    const newDoc = new Y.Text()
+    console.log(newDoc)
+    
+    //documentList.push([newDoc])
+  } 
+
+
+</script>
+
+<button on:click={ ()=> createDoc() }>New Doc!</button>

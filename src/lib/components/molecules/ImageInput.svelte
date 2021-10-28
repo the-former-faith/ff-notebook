ce38fecc46fe55de99c5909874b9c697fbff3db5
Matt Derocher <mjamesderocher@gmail.com>
2021-10-28 11:53:05 +0300

    Added xstate for imageInput

--- src/lib/components/molecules/ImageInput.svelte
+++ src/lib/components/molecules/ImageInput.svelte
@@ -1,19 +1,91 @@
 <script>
+  import { createMachine, assign, interpret } from 'xstate'
 	let files
+  let blob
+
+  $: if(files) blob = URL.createObjectURL(files[0])
+
+  //$: console.log(blob)
+
+  const fileMachine = createMachine({
+    id: 'fileMachine',
+    initial: 'loading',
+    context: {
+      db: undefined,
+      error: undefined
+    },
+    states: {
+      idle: {
+      },
+      loading: {
+        invoke: {
+          id: 'loadIndexedDB',
+          src: (context, event) => (send)=> {
+            const request = indexedDB.open('fileUploads', 2)
+
+            request.onupgradeneeded = function(event) {
+              const db = event.target.result
+
+              db.createObjectStore('files', {keyPath: 'id'})
+
+              console.log("I'm gonna take the upgrade")
+            }
+
+            request.onsuccess = function(event) {
+              send({
+                type: 'SUCCESS',
+                db: event.target.result,
+              })
+            }
+
+          },
+        },
+        on: {
+          SUCCESS: {
+            target: 'success',
+            actions: [
+              assign({
+                db: (ctx, evt) => evt.db,
+              })
+            ]
+          }
+        }
+      },
+      success: {
+        on: {
+          'NEW_UPLOAD': {
+            actions: [
+              ()=> console.log('uploading')
+            ]
+          }
+        }
+      },
+    }
+  })
+
+  const service = interpret(fileMachine).onTransition((state) => {console.log(state)}).start()
+
 </script>
 
+<p>{$service.can('NEW_UPLOAD')}</p>
+
+<img src={blob} alt="New upload" />
+
 <label for="image">Choose a profile picture:</label>
+
 <input
   type="file"
   id="image"
   name="image"
   accept="image/*"
-  label="hey"
   bind:files
-  on:change={(e) => console.log(e)}
 />
 
 <style>
+  img {
+    width: 100%;
+  }
+
   input[type=file] {
     font-size: inherit;
   }

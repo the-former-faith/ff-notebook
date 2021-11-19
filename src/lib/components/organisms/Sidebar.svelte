211709b07010a54a4fcdec2953643e679d7fc2ef
Matthew James Derocher <mjamesderocher@gmail.com>
2021-11-18 21:00:12 -0500

    Fixed broken import

--- src/lib/components/organisms/Sidebar.svelte
+++ src/lib/components/organisms/Sidebar.svelte
@@ -1,15 +1,15 @@
 <script>
   import { allDocsOpened } from '$lib/scripts/stores'
   import DetailsSummary from '$lib/components/molecules/DetailsSummary.svelte'
-  import { mainService } from '$lib/scripts/mainMachine'
+  //import { mainService } from '$lib/scripts/mainMachine'
   import { useSelector } from '@xstate/svelte'
   import DocList from '$lib/components/molecules/DocList.svelte'
 
   let collections
 
-  $: if($mainService?.context.rxdb) {
-    collections = useSelector($mainService.context.rxdb, (state) => state.context.collections)
-  }
+  // $: if($mainService?.context.rxdb) {
+  //   collections = useSelector($mainService.context.rxdb, (state) => state.context.collections)
+  // }
 </script>
 
 <aside>

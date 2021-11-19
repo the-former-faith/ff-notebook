9749c2ae8c40471dbf914b9fba7b717aac80be0e
Matthew James Derocher <mjamesderocher@gmail.com>
2021-11-18 20:11:34 -0500

    Added Netlify Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
-/** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-netlify';
+
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
 		target: '#svelte'
 	}
 };

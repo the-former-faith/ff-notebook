d695aca50618ff5c8122fd329189cc03c91a67b1
Matthew James Derocher <mjamesderocher@gmail.com>
2021-11-19 11:08:20 -0500

    Added schema file

---
+++ src/lib/scripts/schema.js
@@ -0,0 +1,151 @@
+/* Marks */
+
+const link = {
+  title: 'link',
+  type: 'object',
+  properties: {
+    type: {
+      "type": "string",
+      "enum": ["link"]
+    },
+    attrs: {
+      type: 'object',
+      properties: {
+        href: {
+          type: 'string',
+          format: 'uri'
+        },
+        target: {
+          "type": "string",
+          "enum": ["", "_self", "_blank", "_parent", "_top"],
+          "component": "radio"
+        }
+      }
+    }
+  }
+}
+
+const bold = {
+  title: 'bold',
+  type: 'object',
+  properties: {
+    type: {
+      "type": "string",
+      "enum": ["bold"]
+    }
+  }
+}
+
+/* Objects */
+const date = {
+  title: 'date',
+  type: 'object',
+  properties: {
+    date: {
+      "type": "number",
+    },
+    test: {
+      type: 'string'
+    }
+  }
+}
+
+const meta = {
+  id: {
+    type: 'string',
+    display: 'private'
+  },
+  title: {
+    type: 'string',
+  },
+  date,
+  body: {
+    type: 'array',
+    component: 'TipTap',
+    items: {
+      anyOf: [{
+        type: 'object',
+        title: 'paragraph',
+        properties: {
+          type: {
+            type: 'string'
+          },
+          content: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                type: {
+                  type: 'string'
+                },
+                marks: {
+                  type: 'array',
+                  items: {
+                    anyOf: [link, bold]
+                  }
+                },
+                text: {
+                  type: 'string'
+                }
+              },
+            }
+          }
+        }
+      }]
+    }
+  },
+  createdAt: {
+    type: 'number',
+    display: 'private'
+  },
+  updatedAt: {
+    type: 'number',
+    display: 'private'
+  },
+}
+
+export const postSchema = {
+  schema: {
+    title: 'post',
+    description: 'an individual note',
+    version: 0,
+    type: 'object',
+    indexes: [
+      'createdAt',
+      'updatedAt'
+    ],
+    primaryKey: 'id',
+    properties: {
+      ...meta
+    },
+    required: ['title'],
+  }
+}
+
+export const imageSchema = {
+  schema: {
+    title: 'image',
+    description: 'an individual note',
+    version: 1,
+    type: 'object',
+    indexes: [
+      'createdAt',
+      'updatedAt'
+    ],
+    primaryKey: 'id',
+    properties: {
+      ...meta,
+      'url': {
+        "type": "string",
+        "format": "uri",
+        "component": "ImageInput"
+      }
+    },
+    required: ['title'],
+  }
+}
+
+export const schemas = {
+  posts: postSchema,
+  images: imageSchema
+} 
